e84d635204d37a971b17a2278bb4473244b377e6
Mukul kumar <mukulji567@gmail.com>
2026-09-19 16:47:01 +0530

    ADD global exception handling

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
 from fastapi.staticfiles import StaticFiles
 from routers.product import router as product_router
 from routers.user import router as user_router
@@ -10,6 +12,33 @@ app=FastAPI(
     title="E-commerce API",
     version="1.0,0"   
 )
+
+@app.exception_handler(SQLAlchemyError)
+async def database_exception_handler(
+    request: Request,
+    exc: SQLAlchemyError
+):
+    return JSONResponse(
+        status_code=500,
+        content={
+            "error": "Database error",
+            "message": "Something went wrong with the database"
+        }
+    )
+
+
+@app.exception_handler(Exception)
+async def general_exception_handler(
+    request: Request,
+    exc: Exception
+):
+    return JSONResponse(
+        status_code=500,
+        content={
+            "error": "Internal server error",
+            "message": "Something went wrong"
+        }
+    )
 
 app.mount("/uploads",
           StaticFiles(directory="uploads"),
